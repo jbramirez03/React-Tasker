3950846c0615df4f25d40d897bb0ef699c70567b
temptado <jason1287712@gmail.com>
2021-10-07 19:35:11 -0400

    added css framework bootstrap for styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  );
+  return <div className="container">Hello world</div>;
 }
 
 export default App;
