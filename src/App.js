58ae41e090aeca37c54344575576e0369cdd0fd6
temptado <jason1287712@gmail.com>
2021-10-07 19:51:43 -0400

    made header component to hold header text aswell as button"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="container">Hello world</div>;
+  return (
+    <div className="container col-lg-8 mt-3 bg-light">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className='row justify-content-center'>
+            <h1 className="col-lg-3">React Tasker</h1>
+            <button className='btn btn-info col-lg-1'>Add</button>
+        </div>
+    )
+}
+
+export default Header
