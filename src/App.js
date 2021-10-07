37f79d9e02c9a0c9c185477723887cec4ea595e4
temptado <jason1287712@gmail.com>
2021-10-07 19:53:53 -0400

    changed parent element in header component to header and resized container width

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="container col-lg-8 mt-3 bg-light">
+    <div className="container col-lg-6 mt-3 bg-light">
       <Header />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
-        <div className='row justify-content-center'>
+        <header className='row justify-content-center'>
             <h1 className="col-lg-3">React Tasker</h1>
             <button className='btn btn-info col-lg-1'>Add</button>
-        </div>
+        </header>
     )
 }
 
