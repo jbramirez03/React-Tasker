6c2ed72c857d8f504441264097a0298beb8db6a5
temptado <jason1287712@gmail.com>
2021-10-07 20:49:43 -0400

    set tasks in app js for global use and for state to be changed from within other components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "./App.css";
 import Header from "./components/Header";
 import Tasks from './components/Tasks';
+import { useState } from 'react';
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Dentist appointment',
+      day: 'Feb 12th at 12:00pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Change car oil',
+      day: 'Apr 9th at 2:00pm',
+      reminder: false
+    },
+    {
+      id: 3,
+      text: 'Saturday class',
+      day: 'Mar 1st at 11:00am',
+      reminder: false
+    }
+  ]);
+
   return (
     <div className="container col-lg-6 mt-3 bg-light">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,4 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Dentist appointment',
-        day: 'Feb 12th at 12:00pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Change car oil',
-        day: 'Apr 9th at 2:00pm',
-        reminder: false
-    },
-    {
-        id: 3,
-        text: 'Saturday class',
-        day: 'Mar 1st at 11:00am',
-        reminder: false
-    }
-]
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
     return (
         <>
             {tasks.map(task => (<h3 key={task.id}>{task.text}</h3>))}
