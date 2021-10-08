a549feb95baaf3f3e879eb28987c227a20bc510c
temptado <jason1287712@gmail.com>
2021-10-07 21:34:43 -0400

    added ternary operator to change border color of task if reminder is set to true

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import { FaTimes } from 'react-icons/fa';
 
 const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className='container mb-3 p-3 text-center border' onDoubleClick={() => onToggle(task.id)}>
+        <div className={`container mb-3 p-3 text-center border ${task.reminder ? 'border-success' : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>{task.text} <FaTimes className='col-lg-2' style={{ color: 'red', cursor: 'pointer' }} onClick={() => { onDelete(task.id) }} /></h3>
             <p>{task.day}</p>
         </div>
