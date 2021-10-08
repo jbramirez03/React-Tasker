6f3aabfd60912eafcc5e1987ff52a659360e8113
temptado <jason1287712@gmail.com>
2021-10-07 20:43:05 -0400

    added tasks component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Tasks from './components/Tasks';
 
 function App() {
   return (
     <div className="container col-lg-6 mt-3 bg-light">
       <Header />
+      <Tasks />
     </div>
   );
 }
