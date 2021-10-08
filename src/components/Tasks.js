cb7c5b62d9e6fe2bb9ef62114a2527b19dbf96a9
temptado <jason1287712@gmail.com>
2021-10-07 20:56:50 -0400

    wrapped tasks in a div for adding margin to top with bs

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,9 +2,9 @@ import Task from './Task';
 
 const Tasks = ({ tasks }) => {
     return (
-        <>
+        <div className='mt-3'>
             {tasks.map(task => (<Task key={task.id} task={task} />))}
-        </>
+        </div>
     )
 }
 
