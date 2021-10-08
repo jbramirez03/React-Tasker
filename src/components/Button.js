27c9d7769b5e87300dd77da6a2be42c196c56965
temptado <jason1287712@gmail.com>
2021-10-07 20:26:34 -0400

    set up default props for button component as well as prop types

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
+import PropTypes from 'prop-types'
+
+
 const Button = ({ title, styling }) => {
     return (
         <button className={styling}>{title}</button>
     )
+}
+
+Button.defaultProps = {
+    styling: 'btn col-lg-1 btn-info'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    styling: PropTypes.string
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className='row justify-content-center'>
             <h1 className="col-lg-7">React Tasker</h1>
-            <Button title='add' styling='btn btn-info col-lg-1' />
+            <Button title='add' styling='btn col-lg-1 btn-info' />
         </header>
     )
 }
