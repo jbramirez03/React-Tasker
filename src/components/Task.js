577350468f8026876d8d616098faa7c41501319e
temptado <jason1287712@gmail.com>
2021-10-07 21:02:57 -0400

    added styling to fix margins on tasks divs

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import { FaTimes } from 'react-icons/fa';
 
 const Task = ({ task }) => {
     return (
-        <div className='row p-3'>
-            <h3>{task.text}</h3>
+        <div className='row p-3 text-center'>
+            <h3>{task.text} <FaTimes className='col-lg-2' style={{ color: 'red', cursor: 'pointer' }} /></h3>
             <p>{task.day}</p>
         </div>
     )
