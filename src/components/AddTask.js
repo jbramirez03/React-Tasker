59d3056912707d936a949fb5479740344f9af693
temptado <jason1287712@gmail.com>
2021-10-08 19:53:07 -0400

    added onChange funcs to input values in add task form inputs:

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 
 const AddTask = () => {
+    const [text, setText] = useState('');
+    const [day, setDay] = useState('');
+    const [reminder, setReminder] = useState('false');
+
+
     return (
         <form>
             <div>
                 <label>Reminder name</label>
-                <input type="text" className="form-control" placeholder="Haircut appointment..." />
+                <input type="text" className="form-control" placeholder="Haircut appointment..." value={text} onChange={e => setText(e.target.value)} />
             </div>
             <div>
                 <label>Day and Time</label>
-                <input type="text" className="form-control" placeholder="Sep 12th at 1:00pm" />
+                <input type="text" className="form-control" placeholder="Sep 12th at 1:00pm" value={day} onChange={e => setDay(e.target.value)} />
             </div>
             <div class="form-check">
-                <input type="checkbox" className="form-check-input" />
+                <input type="checkbox" className="form-check-input" value={reminder} onChange={e => setReminder(e.currentTarget.checked)} />
                 <label className="form-check-label">Set reminder</label>
             </div>
             <button type="submit" value='Save Task' className="btn btn-primary">Submit</button>
