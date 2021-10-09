8b4163c448871f8a4e54dd036fad4e1f35f78829
temptado <jason1287712@gmail.com>
2021-10-08 20:25:28 -0400

    made button text and color dynamic based on toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import Button from './Button';
 
-const Header = ({ onAdd }) => {
+const Header = ({ onAdd, currentShow }) => {
     return (
         <header className='row justify-content-center border-bottom'>
             <h1 className="col-lg-7">React Tasker</h1>
-            <Button title='add' styling='btn col-lg-1 btn-info' onClick={onAdd} />
+            <Button title={currentShow ? 'Close' : 'Add'} styling={currentShow ? 'btn col-lg-1 btn-danger' : 'btn col-lg-1 btn-info'} onClick={onAdd} />
         </header>
     )
 }
