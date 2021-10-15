1b2243756e691cb0f33fb97a57f110ea4217ded3
temptado <jason1287712@gmail.com>
2021-10-14 23:46:20 -0400

    added icon to navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
+import { RiListCheck2 } from "react-icons/ri";
 
 const Nav = () => {
     return (
         <header>
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="https://reactjs.org/" target='_blank' rel="noreferrer">
-                    React Tasker
-            </a>
+                <a className="navbar-brand" href="https://github.com/jbramirez03/React-Tasker" target='_blank' rel="noreferrer">
+                    React Tasker <span><RiListCheck2 /></span>
+                </a>
             </nav>
         </header>
     )
