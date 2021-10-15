6b7f73db16967dc9dc504912912016e8e833a3c9
temptado <jason1287712@gmail.com>
2021-10-15 00:02:54 -0400

    set positioning for icons in navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,8 +7,10 @@ const Nav = () => {
                 <a className="navbar-brand" href="https://github.com/jbramirez03/React-Tasker" target='_blank' rel="noreferrer">
                     React Tasker <span><RiListCheck2 /></span>
                 </a>
-                <a className='navbar-brand' href="https://www.instagram.com/temptado/" targer='_blank' rel='noreferrer'><RiInstagramLine /></a>
-                <a className='navbar-brand' href="https://github.com/jbramirez03" targer='_blank' rel='noreferrer'><RiGithubFill /></a>
+                <div className='row justify-content-end'>
+                    <a className='navbar-brand justify-content-end col-lg-4' href="https://www.instagram.com/temptado/" targer='_blank' rel='noreferrer'><RiInstagramLine /></a>
+                    <a className='navbar-brand justify-content-end col-lg-4' href="https://github.com/jbramirez03" targer='_blank' rel='noreferrer'><RiGithubFill /></a>
+                </div>
             </nav>
         </header>
     )
