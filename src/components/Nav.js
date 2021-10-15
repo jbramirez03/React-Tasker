bd59a1b0bab440ff86e6458aff0287bda75890b0
temptado <jason1287712@gmail.com>
2021-10-15 00:07:36 -0400

    added linkedIn icon and set postioning

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import { RiListCheck2, RiInstagramLine, RiGithubFill } from "react-icons/ri";
+import { RiListCheck2, RiInstagramLine, RiGithubFill, RiLinkedinBoxFill } from "react-icons/ri";
 
 const Nav = () => {
     return (
@@ -8,8 +8,9 @@ const Nav = () => {
                     React Tasker <span><RiListCheck2 /></span>
                 </a>
                 <div className='row justify-content-end'>
-                    <a className='navbar-brand justify-content-end col-lg-4' href="https://www.instagram.com/temptado/" targer='_blank' rel='noreferrer'><RiInstagramLine /></a>
-                    <a className='navbar-brand justify-content-end col-lg-4' href="https://github.com/jbramirez03" targer='_blank' rel='noreferrer'><RiGithubFill /></a>
+                    <a className='navbar-brand justify-content-end col-lg-2' href="https://www.instagram.com/temptado/" targer='_blank' rel='noreferrer'><RiInstagramLine /></a>
+                    <a className='navbar-brand justify-content-end col-lg-2' href="https://github.com/jbramirez03" targer='_blank' rel='noreferrer'><RiGithubFill /></a>
+                    <a className='navbar-brand justify-content-end col-lg-2' href="https://www.linkedin.com/in/jason-barrera-ramirez-b2a473204/" targer='_blank' rel='noreferrer'><RiLinkedinBoxFill /></a>
                 </div>
             </nav>
         </header>
