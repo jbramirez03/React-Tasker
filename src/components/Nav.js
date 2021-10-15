3770fdd3f562474246c05110e20775e4e6687428
temptado <jason1287712@gmail.com>
2021-10-14 23:53:06 -0400

    added instagram and github icons with links in navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import { RiListCheck2 } from "react-icons/ri";
+import { RiListCheck2, RiInstagramLine, RiGithubFill } from "react-icons/ri";
 
 const Nav = () => {
     return (
@@ -7,6 +7,8 @@ const Nav = () => {
                 <a className="navbar-brand" href="https://github.com/jbramirez03/React-Tasker" target='_blank' rel="noreferrer">
                     React Tasker <span><RiListCheck2 /></span>
                 </a>
+                <a className='navbar-brand' href="https://www.instagram.com/temptado/" targer='_blank' rel='noreferrer'><RiInstagramLine /></a>
+                <a className='navbar-brand' href="https://github.com/jbramirez03" targer='_blank' rel='noreferrer'><RiGithubFill /></a>
             </nav>
         </header>
     )
